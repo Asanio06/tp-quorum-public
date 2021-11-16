08431d30e918949a05ab93a76147037c86e3d0ca
Amaury CLEMENT <amaury.clement@epfedu.fr>
2021-11-16 14:01:24 +0100

    exo4.2.2

--- contracts/MilkDelivery.sol
+++ contracts/MilkDelivery.sol
@@ -10,14 +10,15 @@ contract MilkDelivery {
     address public dairyAddress;
 
     event MilkDelivered(address indexed milkDeliveryAddress,
-                        address indexed milkProducer,
-                        address dairyAddress, 
-                        uint32 liters, uint32 price);
+        address indexed milkProducer,
+        address dairyAddress,
+        uint32 liters, uint32 price);
 
     constructor(address _dairyAddress) public {
-        // TODO: check that _dairyAddress address (laiterie) is not empty
-        // TODO: keep track of the milk producer (éleveur) in milkProducerAddress 
-        // TODO: keep track of the dairy address (laiterie) in dairyAddress
+        require(_dairyAddress != address(0));
+        milkProducerAddress = msg.sender;
+        dairyAddress = _dairyAddress;
+
         // TODO: initialize dairyApproval to a meaningful value
         // TODO: initialize consumed to a meaningful value
     }
@@ -25,34 +26,40 @@ contract MilkDelivery {
     /// Keeps track of a milk delivery
     /// @dev should only be called by the milk producers of this contract
     function sendMilk(uint32 _liters, uint32 _price) public onGoing() {
-        // TODO: keep track of the number of liters sent and the price
-        // TODO: emit a MilkDeliveredEvent
+        price = _price;
+        liters = _liters;
+        emit MilkDelivered(address(this),milkProducerAddress,dairyAddress,_liters,_price);
     }
 
     /// Mark the delivery as accepted by the dairy
     function validateDelivery() onGoing() dairyOnly() public {
         // TODO: implement this!
+        dairyApproval = true;
     }
 
     /// Return true if the delivery was acknowledged by the dairy (laiterie)
     function checkDeliveryApproval() public view returns (bool) {
         // TODO: implement this!
+        return dairyApproval;
     }
 
     /// Mark the delivery as consumed (used in order to make some cheese)
     function consume() external {
+        consumed = true;
         // TODO: implement this!
     }
 
     /// Modifier based on the value of consumed.
     /// @dev if consumed, then the delivery is not onGoing
     modifier onGoing() {
+        require(!consumed);
         _;
         // TODO: implement this!
     }
 
     /// Modifier ensuring that only the dairy of this milk delivery can make a call
     modifier dairyOnly() {
+        require(msg.sender == dairyAddress);
         _;
         // TODO: implement this!
     }
